9bc0b578db3f61e247f7283d31a993e2e46dc611
Haripriya <143163432+Haripriyabellamkonda@users.noreply.github.com>
2023-12-02 19:39:35 +0530

    About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,13 +3,12 @@ import React from 'react';
 import './About.css'; // Import the CSS file for About component
 import { FaLinkedin } from "react-icons/fa";
 
-
 const About = () => {
   return (
     <section id="about">
       <div className="about-container">
         <div className="about-content">
-          <h2>Hi,My name is Haripriya</h2>
+          <h2 className='about-heading'>Hi,My name is Haripriya</h2>
           <p>
             Welcome to my portfolio! I am a passionate and dedicated individual with expertise in
             front-end and back-end technologies.In addition to this,I have cultivated a foundational understanding of AI and ML concepts. With a strong foundation in computer science and hands-on
