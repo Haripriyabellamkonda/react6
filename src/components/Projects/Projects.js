06e72f5b6e7e385869372990c44db432fae34e27
Haripriya <143163432+Haripriyabellamkonda@users.noreply.github.com>
2024-02-14 23:21:32 +0530

    Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ import './Projects.css';
 const projectsData = [
   { id: 1, title: 'Lost and Found Website', description: 'The Lost and Found website is developed by me to tackle real world challenges through innovative solutions that integrate my theoretical knowledge gained in the course with practical application. A lost and found application is a website that helps people report and find lost items. It can be used by individuals, organizations, and businesses.."Lost and Found" website, designed to help individuals for reporting lost items, locating found items, reconnect with their lost belongings.' },
   { id: 2, title: 'Smart Knee braces', description: 'This project was created by me for our engineering clinics which monitors the deformations and movements of the knee and sends an alert to the user. This project was built as a college project with a team of 6 people where my responsibility was to manage and build the circuit with all connections set to proper and develop an application.' },
-  { id: 3, title: 'Task Pilot', description: '•	Task Pilot is a task management application built on the MERN stack This project was created by a team of 5 people during an MERN stack internship My responsibilities include utilizing ReactJs for UI, handling API calls, and managing routing.' }
+  { id: 3, title: 'Task Pilot', description: 'Task Pilot is a task management application built on the MERN stack This project was created by a team of 5 people during an MERN stack internship My responsibilities include utilizing ReactJs for UI, handling API calls, and managing routing.' }
 
   // Add more projects as needed
 ];
